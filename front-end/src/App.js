8dc84150eeaa4bc7a84af328f4a4f25f6ce97006
Vincent Wong <vincentwco2002@gmail.com>
2025-05-13 15:11:10 +0800

    create searchbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router';
 import './App.css';
 import Root from './components/root';
+import Home from './pages/home';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Root />}>
-
+    <Route index element={<Home />} />
   </Route>
 ))
 
---
+++ front-end/src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './home.css';
+import Searchbar from "../../components/searchbar";
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <Searchbar />
+        </div>
+    )
+}
+
+export default Home;
