2cefc8fcaeaa6b722bd413536eaf77e90ce61f93
Vincent Wong <vincentwco2002@gmail.com>
2025-05-13 10:27:57 +0800

    setup router and root

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,16 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router';
 import './App.css';
+import Root from './components/root';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Root />}>
+
+  </Route>
+))
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
