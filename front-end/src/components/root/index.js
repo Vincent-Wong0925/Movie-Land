2b3af69ff0c6c80f7ad056c24bd1c3136b2af4df
Vincent Wong <vincentwco2002@gmail.com>
2025-05-14 10:24:49 +0800

    re-locate searchbar

--- front-end/src/components/root/index.js
+++ front-end/src/components/root/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Navigation from "../navigation";
+import Searchbar from "../searchbar";
 
 const Root = () => {
     return(
         <div className="Root">
             <Navigation />
+            <Searchbar />
             <Outlet />
         </div>
     )
--- front-end/src/pages/home/index.js
+++ front-end/src/pages/home/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import './home.css';
-import Searchbar from "../../components/searchbar";
 
 const Home = () => {
     return (
         <div className="Home">
-            <Searchbar />
+            
         </div>
     )
 }
