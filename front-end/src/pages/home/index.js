5f0d13cacaf8c1c5300e2fe48bc687ac42bc5bd6
Vincent Wong <vincentwco2002@gmail.com>
2025-05-14 13:46:49 +0800

    Create hero section

--- front-end/src/pages/home/index.js
+++ front-end/src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './home.css';
 import FilmSlider from "../../components/slider";
+import Hero from "../../components/hero";
 
 const films = [
     {id: '1', title: 'film title', release_date: '2008-11-21', popularity: 82.5, poster_path: '/1E5baAaEse26fej7uHcjOgEE2t2.jpg'},
@@ -17,6 +18,7 @@ const films = [
 const Home = () => {
     return (
         <div className="Home">
+            <Hero />
             <FilmSlider films={films}/>
         </div>
     )
