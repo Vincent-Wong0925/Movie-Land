954b5fdb1a04175e9aa9820544adc2cc86f00036
Vincent Wong <vincentwco2002@gmail.com>
2025-05-14 11:15:33 +0800

    create card component

---
+++ front-end/src/components/card/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './card.css';
+import { NavLink } from "react-router";
+
+const Card = ({ id, poster, title, release_date, popularity }) => {
+    return (
+        <div className="Card" id={id}>
+            <NavLink className="card-link">
+                <img className="card-img" src={poster} alt="film poster" />
+                <span className="card-info">
+                    <p className="card-title">{title}</p>
+                    <p className="card-date">Release date: {release_date}</p>
+                    <p className="card-popularity">Popularity: {popularity.toFixed(1)}%</p>
+                </span>
+            </NavLink>
+        </div>
+    )
+}
+
+export default Card;
--- front-end/src/pages/home/index.js
+++ front-end/src/pages/home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './home.css';
+import Card from "../../components/card";
 
 const Home = () => {
     return (
         <div className="Home">
-            
+            <Card id="1234" title="Film Title" release_date="2008-11-21" popularity={82.5432} poster="https://image.tmdb.org/t/p/w342/1E5baAaEse26fej7uHcjOgEE2t2.jpg"/>
         </div>
     )
 }
