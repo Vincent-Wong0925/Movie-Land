56034efd323972ea27c85c753c5bc24137bd26a8
Vincent Wong <vincentwco2002@gmail.com>
2025-05-21 10:47:25 +0800

    rename component class

--- front-end/src/components/hero/index.js
+++ front-end/src/components/hero/index.js
@@ -14,7 +14,7 @@ const Hero = () => {
             <div className="hero-content">
                 <h1 className="bold">Welcome to <span className="lime">MovieLand</span></h1>
                 <p>A place to find and rate your favorite movie.</p>
-                <Link><button className="login-btn bold">Login</button></Link>
+                <Link><button className="hero-login-btn bold">Login</button></Link>
             </div>
         </div>
     )
