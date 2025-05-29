692e0ee4324d3d318fb67fda957361e8a5156adf
Vincent Wong <vincentwco2002@gmail.com>
2025-05-29 13:47:01 +0800

    Update .env for database connection

--- back-end/db/index.js
+++ back-end/db/index.js
@@ -2,11 +2,11 @@ const pg = require('pg');
 const { Pool } = pg;
  
 const pool = new Pool({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'movie-site-project',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
