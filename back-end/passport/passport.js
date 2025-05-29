e818e0e4d277c5f153dc83a49227913897f81cb9
Vincent Wong <vincentwco2002@gmail.com>
2025-05-29 16:04:18 +0800

    update oauth redirect uri

--- back-end/passport/passport.js
+++ back-end/passport/passport.js
@@ -27,7 +27,7 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, passwor
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'https://movie-land-server.onrender.com/auth/google/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
     try {
